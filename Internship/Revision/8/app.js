1f5cd667ecb4b98f56f37bbc84fc5268ca1d9949
Yadnesh Ahirrao <yadneshahirrao.19@gmail.com>
2024-01-31 00:53:37 +0530

    change in app.js

--- Internship/Revision/8/app.js
+++ Internship/Revision/8/app.js
@@ -1,28 +1,26 @@
-document.addEventListener("DOMContentLoaded", function () {
-  let postsContainer = document.querySelector(".posts-container");
-  let loadingIndicator = document.querySelector(".loading-indicator");
-  let errorMessage = document.querySelector(".error-message");
+let postsContainer = document.querySelector(".posts-container");
+let loadingIndicator = document.querySelector(".loading-indicator");
+let errorMessage = document.querySelector(".error-message");
 
-  fetch("https://jsonplaceholder.typicode.com/posts")
-    .then((response) => {
-      return response.json();
-    })
-    .then((posts) => {
-      loadingIndicator.style.display = "none";
+fetch("https://jsonplaceholder.typicode.com/posts")
+  .then((response) => {
+    return response.json();
+  })
+  .then((posts) => {
+    loadingIndicator.style.display = "none";
 
-      posts.forEach((post) => {
-        const postCard = document.createElement("div");
-        postCard.classList.add("card");
-        postCard.innerHTML = `
+    posts.forEach((post) => {
+      const postCard = document.createElement("div");
+      postCard.classList.add("card");
+      postCard.innerHTML = `
                     <h2>${post.title}</h2>
                     <p>${post.body}</p>
                 `;
-        postsContainer.appendChild(postCard);
-      });
-    })
-    .catch((error) => {
-      loadingIndicator.style.display = "none";
-      errorMessage.style.display = "block";
-      console.error(error);
+      postsContainer.appendChild(postCard);
     });
-});
+  })
+  .catch((error) => {
+    loadingIndicator.style.display = "none";
+    errorMessage.style.display = "block";
+    console.error(error);
+  });
