85327b5ee5b544d2e88fdcb6a42c18cb3572ce67
Yadnesh Ahirrao <yadneshahirrao.19@gmail.com>
2024-02-08 23:32:58 +0530

    Section 7 end 8th running

---
+++ Internship/Udemy Vue/Section 7/vue-practice/src/components/FriendContact.vue
@@ -0,0 +1,70 @@
+<template>
+  <li>
+    <h2>{{ name }} {{ isFavorite ? "(Favorite)  " : "" }}</h2>
+    <button @click="toggleDetails">
+      {{ detailsAreVisible ? "Hide" : "Show" }} Details
+    </button>
+    <button @click="toggleFavorite">Toggle Favorite</button>
+    <ul v-if="detailsAreVisible">
+      <li><strong>Phone:</strong> {{ phoneNumber }}</li>
+      <li><strong>Email:</strong> {{ emailAddress }}</li>
+    </ul>
+  </li>
+</template>
+
+<script>
+export default {
+  // props: [
+  //   'name',
+  //   'phoneNumber',
+  //   'emailAddress'
+  // ],
+  props: {
+    id: {
+      type: String,
+      required: true,
+    },
+    name: {
+      type: String,
+      required: true,
+    },
+    phoneNumber: {
+      type: String,
+      required: true,
+    },
+    emailAddress: {
+      type: String,
+      required: true,
+    },
+    isFavorite: {
+      type: Boolean,
+      required: false,
+      default: false,
+      // validator: function(value) {
+      //   return value === '1' || value === '0';
+      // }
+    },
+  },
+  data() {
+    return {
+      detailsAreVisible: false,
+      // friendIsFavorite: this.isFavorite,
+      // friends: {
+      //   id: "shreyash",
+      //   name: "Shreyash Patil",
+      //   phone: "01234 5678 991",
+      //   email: "shreyash@localhost.com",
+      // },
+    };
+  },
+
+  methods: {
+    toggleDetails() {
+      this.detailsAreVisible = !this.detailsAreVisible;
+    },
+    toggleFavorite() {
+      this.$emit("toggle-favorite", this.id);
+    },
+  },
+};
+</script>
