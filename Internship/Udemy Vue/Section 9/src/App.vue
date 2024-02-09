fd635c4b2bee342b17916ea90d52ad8ed970364f
Yadnesh Ahirrao <yadneshahirrao.19@gmail.com>
2024-02-09 22:25:33 +0530

    update

--- Internship/Udemy Vue/Section 9/src/App.vue
+++ Internship/Udemy Vue/Section 9/src/App.vue
@@ -8,26 +8,34 @@
       :info-text="activeUser.description"
       :role="activeUser.role"
     ></user-info>
+    <cource-goals>
+      <template #default="slotProps">
+        <h2>{{ slotProps.item }}</h2>
+        <p>{{ slotProps['another-prop'] }}</p>
+      </template>
+    </cource-goals>
   </div>
 </template>
 
 <script>
-import TheHeader from './components/TheHeader.vue';
-import BadgeList from './components/BadgeList.vue';
-import UserInfo from './components/UserInfo.vue';
+import TheHeader from "./components/TheHeader.vue";
+import BadgeList from "./components/BadgeList.vue";
+import UserInfo from "./components/UserInfo.vue";
+import CourceGoals from "./components/CourceGoals.vue";
 
 export default {
   components: {
     TheHeader,
     BadgeList,
-    UserInfo
+    UserInfo,
+    CourceGoals,
   },
   data() {
     return {
       activeUser: {
-        name: 'Maximilian Schwarzmüller',
-        description: 'Site owner and admin',
-        role: 'admin',
+        name: "Maximilian Schwarzmüller",
+        description: "Site owner and admin",
+        role: "admin",
       },
     };
   },
