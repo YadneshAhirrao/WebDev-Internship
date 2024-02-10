0a2bc2c07a804297353cd463ba8a1aea7472fb07
Yadnesh Ahirrao <yadneshahirrao.19@gmail.com>
2024-02-10 22:45:58 +0530

    Section 10 Started

---
+++ Internship/Udemy Vue/Section 10/src/App.vue
@@ -0,0 +1,49 @@
+<template>
+  <the-header title="Remember Me"></the-header>
+  <stored-resource :resources="storedResource"></stored-resource>
+</template>
+
+<script>
+import StoredResource from './components/learning-resources/StoredResource.vue';
+import TheHeader from './components/layouts/TheHeader.vue';
+export default {
+  components: {
+    StoredResource,
+    TheHeader,
+  },
+  data() {
+    return {
+      storedResource: [
+        {
+          id: 'official-guide',
+          title: 'Official Guide',
+          description: 'The Official Vue.js Documentation',
+          link: 'https://vuejs.org/',
+        },
+        {
+          id: 'google',
+          title: 'Google',
+          description: 'Learn how to google...',
+          link: 'https://google.com/',
+        },
+      ],
+    };
+  },
+};
+</script>
+
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap');
+
+* {
+  box-sizing: border-box;
+}
+
+html {
+  font-family: 'Roboto', sans-serif;
+}
+
+body {
+  margin: 0;
+}
+</style>
