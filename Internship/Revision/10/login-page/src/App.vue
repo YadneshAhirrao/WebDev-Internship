a20263027976b424c6b7a5d03311ba9cb6feafd0
Yadnesh Ahirrao <yadneshahirrao.19@gmail.com>
2024-02-12 13:58:44 +0530

    Login Page Added Sample project deleted

---
+++ Internship/Revision/10/login-page/src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <login-page> </login-page>
+</template>
+
+<script>
+import LoginPage from "./components/LoginPage.vue";
+export default {
+  components: {
+    LoginPage,
+  },
+};
+</script>
+
+<style>
+*{
+	padding: 0;
+	margin: 0;
+	box-sizing: border-box;
+}
+
+body{
+    font-family: 'Poppins', sans-serif;
+    overflow: hidden;
+}
+</style>
