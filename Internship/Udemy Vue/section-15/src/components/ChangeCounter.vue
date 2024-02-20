3b73b14801f72d47fff22d78d5c20c4d09d459d2
Yadnesh Ahirrao <yadneshahirrao.19@gmail.com>
2024-02-20 12:15:57 +0530

    Section 15

---
+++ Internship/Udemy Vue/section-15/src/components/ChangeCounter.vue
@@ -0,0 +1,17 @@
+<template>
+  <button @click="increment">Add One</button>
+  <button @click="increase({value: 12})">Add 12</button>
+</template>
+
+<script>
+import { mapActions } from 'vuex';
+export default {
+  methods: {
+    // addOne() {
+    //   this.$store.dispatch("increment");
+    // },
+
+    ...mapActions(['increment', 'increase'])
+  },
+};
+</script>
