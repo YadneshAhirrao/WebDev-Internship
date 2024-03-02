2249faf87d2b7581e698bff847c0d466391630ce
Yadnesh Ahirrao <yadneshahirrao.19@gmail.com>
2024-03-03 00:53:47 +0530

    section-19-b completed

--- Internship/Udemy Vue/section-19-b/src/App.vue
+++ Internship/Udemy Vue/section-19-b/src/App.vue
@@ -6,27 +6,37 @@
 </template>
 
 <script>
-import USER_DATA from './dummy-data.js';
+import { ref } from "vue";
+import USER_DATA from "./dummy-data.js";
 
-import UserList from './components/users/UserList.vue';
-import ProjectsList from './components/projects/ProjectsList.vue';
+import UserList from "./components/users/UserList.vue";
+import ProjectsList from "./components/projects/ProjectsList.vue";
 
 export default {
   components: {
     UserList,
     ProjectsList,
   },
-  data() {
-    return {
-      selectedUser: null,
-      activeUsers: USER_DATA,
-    };
+  setup() {
+    const selectedUser = ref(null);
+    const activeUsers = USER_DATA;
+
+    function selectUser(uid) {
+      selectedUser.value = activeUsers.find((usr) => usr.id === uid);
+    }
+    return { selectedUser, activeUsers, selectUser };
   },
-  methods: {
-    selectUser(uid) {
-      this.selectedUser = this.activeUsers.find((usr) => usr.id === uid);
-    },
-  },
+  // data() {
+  //   return {
+  //     selectedUser: null,
+  //     activeUsers: USER_DATA,
+  //   };
+  // },
+  // methods: {
+  //   selectUser(uid) {
+  //     this.selectedUser = this.activeUsers.find((usr) => usr.id === uid);
+  //   },
+  // },
 };
 </script>
 
